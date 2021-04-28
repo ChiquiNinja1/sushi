c8d52671a8a92487fcd6102ebc0267b9f24cc669
Santiago Linares <e.linares@donboscosandona.it>
2021-04-28 10:22:27 +0000

    index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/home/index'
+  get '/home/index', to: 'home#index' as: 'index'
+  get '/about', to: 'home#chi_siamo' as: 'about'
+
+  root 'home#index'
 end
