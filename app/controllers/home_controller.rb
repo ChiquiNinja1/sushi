10df3540df3265d6a639ff51f69e14eb914274e1
Santiago Linares <e.linares@donboscosandona.it>
2021-05-05 09:55:01 +0000

    email

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
     before_action :authenticate_user
-    before_action :set_sushi, only: %i[ show edit update destroy ]
-
-
+    
     def index
     end
 
